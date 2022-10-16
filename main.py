77d55db2a1e3f96d31b2de956ccd93607d21792d
Nikhil Balbadri <nBalbadri@boylesports.com>
2022-10-16 19:56:42 +0100

    test

--- main.py
+++ main.py
@@ -77,7 +77,6 @@ def loopFunction():
 def hosting():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usuage")
     chrome_options.add_argument("--no-sandbox")
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
